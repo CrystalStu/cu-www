73e388b7c999db817fdc2427b0f52fd9f0683c98
TURX <turuixuan@foxmail.com>
2018-08-20 16:33:35 +0800

    Update status on Aug 20, 2018.

--- public/js/cooperation.js
+++ public/js/cooperation.js
@@ -1,6 +1,6 @@
-var typed_embrace = new Typed('#embrace', {
+/*var typed_embrace = new Typed('#embrace', {
     typeSpeed: 50,
     backSpeed: 30,
     loop: true,
     strings: ["<em>research</em>", "brain", "<b>source</b>", "<b>ORGANIZATION</b>"]
-});
+});*/
